7dd5293c7e414e09e58869ac45a27d83dfeee2e7
Alec <alecjost7@gmail.com>
2024-10-09 17:34:11 -0500

    format update to pass tests

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -44,7 +44,7 @@ public class TechJobs {
 
                     ArrayList<String> results = JobData.findAll(columnChoice);
 
-                    System.out.println("\n*** All " + columnChoices.get(columnChoice) + " Values ***");
+                    System.out.println("***** All " + columnChoices.get(columnChoice) + " Values *****\n");
 
                     // Print list of skills, employers, etc
                     for (String item : results) {
@@ -87,7 +87,7 @@ public class TechJobs {
 
         do {
 
-            System.out.println("\n" + menuHeader);
+            System.out.println(menuHeader);
 
             // Print available choices
             for (int j = 0; j < choiceKeys.length; j++) {
